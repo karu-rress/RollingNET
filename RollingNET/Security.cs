08ade598b5ee7b5cd0d3c25f4f9d60cccd1ab5e5
Karu <nsun527@naver.com>
2022-08-15 15:30:11 +0900

    newest

--- RollingNET/Security.cs
+++ RollingNET/Security.cs
@@ -5,25 +5,7 @@ using System.Text;
 
 namespace RollingRess.Security;
 
-[Obsolete("Use Encryptor class instead.")]
-public static class Security
-{
-    public static string SHA256Encrypt(string input)
-    {
-        using SHA256 sha256 = SHA256.Create();
-        byte[] encryptBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(input));
-        return Convert.ToBase64String(encryptBytes);
-    }
-
-    public static string SHA512Encrypt(string input)
-    {
-        using SHA512 sha256 = SHA512.Create();
-        byte[] encryptBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(input));
-        return Convert.ToBase64String(encryptBytes);
-    }
-}
-
-public static class Encyptor
+public static class Encryptor
 {
     public static string SHA256(string input)
     {
